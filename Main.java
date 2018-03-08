148bde1583f20d77dcf6f369dcbe9230907e1e17
Maxutbek <36811494+Maxutbek@users.noreply.github.com>
2018-03-08 15:51:00 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
         String x = "cat-fish-rabbit-bird";
         String [] myAnimals = x.split("-");
-        System.out.println(myAnimals[2]);
+        System.out.println(myAnimals[1]);
     }
 }
